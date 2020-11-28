e9baace0687f38dc94bcbbd0b4bbe56cb6c7698b
OnRJ <chugu@bk.ru>
2020-11-28 14:21:50 +0300

    add test admin login

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,14 +1,30 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Tests {
+    WebDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "/Users/ruathn7/Documents/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
 
     @Test
-    public void testOpenLink() {
-        System.setProperty("webdriver.chrome.driver", "/Users/ruathn7/Documents/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        driver.quit();
+    public void testAdminLogin() {
+        driver.get("http://192.168.64.2/litecart/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("remember_me")).click();
+        driver.findElement(By.name("login")).click();
     }
 }

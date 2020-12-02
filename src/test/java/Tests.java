832fde42057e64bc864e9aa8f403a5d22658cd11
OnRJ <chugu@bk.ru>
2020-12-03 00:46:13 +0300

    add test check list

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -3,15 +3,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Tests {
     WebDriver driver;
 
     @Before
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "/Users/ruathn7/Documents/chromedriver");
-        driver = new ChromeDriver();
+        System.setProperty("webdriver.gecko.driver", "/Users/ruathn7/Documents/geckodriver");
+        System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
+        driver = new FirefoxDriver();
     }
 
     @After
@@ -20,11 +21,24 @@ public class Tests {
     }
 
     @Test
-    public void testAdminLogin() {
+    public void testCheckList() {
         driver.get("http://192.168.64.2/litecart/admin");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("remember_me")).click();
         driver.findElement(By.name("login")).click();
+
+        int sizeList = driver.findElements(By.cssSelector("ul#box-apps-menu > li#app-")).size();
+        for (int i = 1; i <= sizeList; i++) {
+            driver.findElement(By.cssSelector("ul#box-apps-menu > li#app-:nth-child(" + i + ")")).click();
+            driver.findElement(By.cssSelector("td#content > h1")).isDisplayed();
+
+            int sizeChildsList = driver.findElements(By.cssSelector("ul#box-apps-menu > li > ul.docs li")).size();
+
+            for (int j = 1; j <= sizeChildsList; j++) {
+                driver.findElement(By.cssSelector("ul#box-apps-menu > li > ul li:nth-child(" + j + ")")).click();
+                driver.findElement(By.cssSelector("td#content > h1")).isDisplayed();
+            }
+        }
     }
 }

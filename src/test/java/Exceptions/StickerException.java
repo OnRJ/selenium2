abf55b1a38cead51d0e25fae5c45ab54b6dcdc92
OnRJ <chugu@bk.ru>
2020-12-05 21:09:57 +0300

    added test check stickers

---
+++ src/test/java/Exceptions/StickerException.java
@@ -0,0 +1,12 @@
+package Exceptions;
+
+public class StickerException extends RuntimeException {
+
+    public StickerException() {
+        super();
+    }
+
+    public StickerException(String message) {
+        super(message);
+    }
+}
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,3 +1,4 @@
+import Exceptions.StickerException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -38,6 +39,58 @@ public class Tests {
             for (int j = 1; j <= sizeChildsList; j++) {
                 driver.findElement(By.cssSelector("ul#box-apps-menu > li > ul li:nth-child(" + j + ")")).click();
                 driver.findElement(By.cssSelector("td#content > h1")).isDisplayed();
+            }
+        }
+    }
+
+    @Test
+    public void testCheckStickers(){
+        driver.get("http://192.168.64.2/litecart/en/");
+
+        // Проверка стикеров в блоке "Most Popular"
+        int sizeListMostPopular = driver.findElements(By
+                .cssSelector("div#box-most-popular li.product.column.shadow.hover-light"))
+                .size();
+
+        for (int i = 1; i <= sizeListMostPopular; i++) {
+            int countStickers = driver.findElements(By
+                    .cssSelector("div#box-most-popular li.product.column.shadow.hover-light:nth-child(" + i + ") div.sticker"))
+                    .size();
+
+            if (countStickers > 1){
+                throw new StickerException("Неверное кол-во стикеров на товаре №" + i + " в разделе \"Most Popular\"");
+            }
+        }
+
+
+        // Проверка стикеров в блоке "Campaigns"
+        int sizeListCampaigns = driver.findElements(By
+                .cssSelector("div#box-campaigns li.product.column.shadow.hover-light"))
+                .size();
+
+        for (int i = 1; i <= sizeListCampaigns; i++) {
+            int countStickers = driver.findElements(By
+                    .cssSelector("div#box-campaigns li.product.column.shadow.hover-light:nth-child(" + i + ") div.sticker"))
+                    .size();
+
+            if (countStickers > 1){
+                throw new StickerException("Неверное кол-во стикеров на товаре №" + i + " в разделе \"Campaigns\"");
+            }
+        }
+
+
+        // Проверка стикеров в блоке "Latest products"
+        int sizeListLatestProducts = driver.findElements(By
+                .cssSelector("div#box-latest-products li.product.column.shadow.hover-light"))
+                .size();
+
+        for (int i = 1; i <= sizeListLatestProducts; i++) {
+            int countStickers = driver.findElements(By
+                    .cssSelector("div#box-latest-products li.product.column.shadow.hover-light:nth-child(" + i + ") div.sticker"))
+                    .size();
+
+            if (countStickers > 1){
+                throw new StickerException("Неверное кол-во стикеров на товаре №" + i + " в разделе \"Latest products\"");
             }
         }
     }
